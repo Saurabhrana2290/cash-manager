7a038a23b776c6b117ef29bfe3e2ae11b0239ef5
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-09 22:08:40 +0530

    Created structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  var [billAmountValue,setBillAmountValue]=useState(0);
+  function billAmountHandler(event){
+    billAmountValue = event.target.value;
+    setBillAmountValue(billAmountValue);
+  }
+  function cashGivenHandler(event){
+    var cashGivenValue = event.target.value;
+    return cashGivenValue;
+  }
+  function onClickHandler(){
+    billAmountHandler();
+    cashGivenHandler();
+    if(billAmountValue > 0){
+
+    }else{
+
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Cash Register Manager</h1>
+      <p className="description">Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
+      <label htmlFor="billAmount">Bill Amount:</label>
+      <input onChange={billAmountHandler} type="text" className="billAmount"/>
+      <label htmlFor="cashGiven">Cash Given:</label>
+      <input onChange={cashGivenHandler} type="text" className="cashGiven" />
+      <button onClick={onClickHandler} className="checkButton">Check</button>
+      <div className="message"></div>
+      <table>
+        <caption className="tableCaption">Return Change</caption>
+        <tr>
+         
+        </tr>
+        <tr>
+          <th>Note</th>
+          <th>2000</th>
+          <th>500</th>
+          <th>100</th>
+          <th>20</th>
+          <th>10</th>
+          <th>5</th>
+          <th>1</th>
+        </tr>
+      </table>
     </div>
   );
 }
